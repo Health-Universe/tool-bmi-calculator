bacd307e48ea26edbc3fda30d5e8d4f22b125716
Greyson Lalonde <greyson.r.lalonde@gmail.com>
2024-10-24 17:59:27 -0400

    add note on usage

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ class BMICalculationInput(BaseModel):
         ge=0.1,
         examples=[1.75],
         description=(
-            "Enter your height."
+            "Enter your height in feet if using imperial or meters if using metric."
         ),
     )
     weight: float = Field(
@@ -46,7 +46,7 @@ class BMICalculationInput(BaseModel):
         ge=0.1,
         examples=[70.0],
         description=(
-            "Enter your weight."
+            "Enter your weight pounds if using imperial or kilograms if using metric."
         ),
     )
 
